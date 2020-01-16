486901c49fc721b4ca7f774880edfe0819621c1b
willensoll <william.ensoll@gmail.com>
2020-01-16 21:02:44 +0000

    Add menu

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,8 +3,7 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
     public static void main(String[] args) {
     Prompter prompter = new Prompter();
-    prompter.welcomeMessage();
-    Books books = new Books();
-    prompter.printBooks(books.getBooks());
+    prompter.printWelcome();
+    prompter.initMenu();
     }
 }
--- src/com/twu/biblioteca/Prompter.java
+++ src/com/twu/biblioteca/Prompter.java
@@ -1,13 +1,34 @@
 package com.twu.biblioteca;
-
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Prompter {
-    public void welcomeMessage() {
+
+    private Scanner sc = new Scanner(System.in);
+
+    public void printWelcome() {
         System.out.println("Welcome to Biblioteca, your one-stop-shop for great book titles in Bangalore!");
     }
 
-    public void printBooks(ArrayList<Book> bookList) {
+    public void initMenu() {
+        System.out.println("*** Main Menu ***");
+        System.out.println("1 - List of Books");
+        System.out.print("Please select an option by number: ");
+        int selection = sc.nextInt();
+        printSelection(selection);
+    }
+
+    private void printSelection(int selection) {
+        switch (selection) {
+            case 1:
+                Books books = new Books();
+                printBooks(books.getBooks());
+        }
+    }
+
+    private void printBooks(ArrayList<Book> bookList) {
+        System.out.println("*** Showing Books ***");
+        System.out.println("*** Name | Author | Publish Date ***");
         for (Book book: bookList) {
             System.out.println(book.toListing());
         }
