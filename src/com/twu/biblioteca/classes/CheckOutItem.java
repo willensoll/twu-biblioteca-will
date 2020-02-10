ea4d1d3ec22fb4f33a0398ca2c2973107c0c4a7f
willensoll <william.ensoll@gmail.com>
2020-02-10 09:47:42 +0000

    create checkoutitemclass

---
+++ src/com/twu/biblioteca/classes/CheckOutItem.java
@@ -0,0 +1,36 @@
+package com.twu.biblioteca.classes;
+
+import com.twu.biblioteca.interfaces.IPrompter;
+
+import javax.naming.InvalidNameException;
+import java.util.ArrayList;
+
+public class CheckOutItem extends LibraryAction {
+
+    private IPrompter _I_prompter;
+    private ArrayList<LibraryItem> _itemList;
+    private IPrompter IPrompterMock;
+    private String _itemName;
+
+
+    public CheckOutItem(IPrompter prompter, ArrayList itemList, String itemType) {
+        super(itemList);
+        _I_prompter = prompter;
+        _itemList = itemList;
+        _itemName = itemType;
+    }
+
+    public void checkOut() {
+        _I_prompter.print("Enter name of "+ _itemName +" to borrow: ");
+        String itemToCheckOut = _I_prompter.readInput();
+        try {
+            LibraryItem item = super.validateItemRequest(itemToCheckOut, true);
+            super.amendItemAvailabilityAfterCheckout(item);
+            _I_prompter.printWithNewLine("Thank you! Enjoy the " + _itemName);
+        } catch (InvalidNameException exception) {
+            _I_prompter.printWithNewLine("*** Sorry, that " + _itemName + " is not available ***");
+        }
+    }
+
+
+}
---
+++ test/com/twu/biblioteca/checkOutTests.java
@@ -0,0 +1,49 @@
+package com.twu.biblioteca;
+
+import com.twu.biblioteca.classes.Book;
+import com.twu.biblioteca.classes.CheckOutItem;
+import com.twu.biblioteca.classes.Prompter;
+import com.twu.biblioteca.interfaces.IPrompter;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.*;
+
+public class checkOutTests {
+
+    private CheckOutItem _checkOutItem;
+    private IPrompter IPrompterMock;
+    private ArrayList itemList;
+    private Book testBook;
+    private Book testBook2;
+
+
+
+    @Before
+    public void setUp() {
+        IPrompterMock = mock(IPrompter.class);
+        itemList = new ArrayList<>();
+        testBook = new Book("Catcher in the Rye", "J D Salinger", 1951);
+        testBook2 = new Book("Sum", "David Eagleman", 2009);
+        itemList.add(testBook);
+        itemList.add(testBook2);
+        _checkOutItem = new CheckOutItem(IPrompterMock, itemList,"book");
+    }
+
+    @Test
+    public void checkoutItem_checks_out_successfuly() {
+        when(IPrompterMock.readInput()).thenReturn("Catcher in the rye");
+        _checkOutItem.checkOut();
+        verify(IPrompterMock).printWithNewLine("Thank you! Enjoy the book");
+
+    }
+
+    @Test
+    public void unsuccessful_checkout_prints_correct_messages() {
+        when(IPrompterMock.readInput()).thenReturn("Will");
+        _checkOutItem.checkOut();
+        verify(IPrompterMock).printWithNewLine("*** Sorry, that book is not available ***");
+    }
+}
